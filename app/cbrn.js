72fd184545853632dd4eae9fd8744a6a4c1a1706
Ilja Biryukov <uncle.funkay@gmail.com>
2022-07-07 18:58:12 +0300

    update options

--- app/cbrn.js
+++ app/cbrn.js
@@ -10,18 +10,18 @@ let args = require('minimist')(process.argv.slice(2));
 
 let options = {
   dir: args.d ? args.d : './',
-  zip: args.zip
+  zip: args.a || args.arch || args.zip,
 };
 
-if (args.p) {
+if (args.p || args.prep) {
   prep(options);
-} else if (!args.h) {
+} else if (!args.h || args.help) {
   issues(options);
 } else {
-  console.log('Usage: cbrn [options]');
-  console.log('Options:');
-  console.log('  -p - Prep.');
-  console.log('  -d path - Path to the comic books folders. By default current directory is used.');
-  console.log('  --zip - Archive renamed issues.');
-  console.log('  -h - Help.');
+  console.log("Usage: cbrn [options]");
+  console.log("Options:");
+  console.log("  -d path - Path to the comic books folders. By default current directory is used.");
+  console.log("  -p, --prep - Prepare archives for processing.");
+  console.log("  -a, --arch - Archive renamed issues.");
+  console.log("  -h, --help - Help.");
 }
